06cfa60bf1c156348f0a5fb44568c90d4f313d2c
Salman-S7 <sammyzed1505@gmail.com>
2024-07-06 22:53:46 +0530

    initialised project

---
+++ app/about/[id]/page.tsx
@@ -0,0 +1,12 @@
+import { useRouter } from 'next/navigation'
+import React from 'react'
+
+const page = () => {
+    const router = use();
+    const { id } = ;
+  return (
+      <div>Page with some id { id }</div>
+  )
+}
+
+export default page
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="">
+      <h1>Hii there</h1>
+    </div>
+  );
+}
