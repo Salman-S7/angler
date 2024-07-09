65786297d9088c8b6438748c40616d285a86c76e
Salman-S7 <sammyzed1505@gmail.com>
2024-07-09 18:00:40 +0530

    Book a demo section complete

---
+++ components/BookADemo.tsx
@@ -0,0 +1,29 @@
+import { MovingBorders } from "./ui/moving-borders";
+
+const BookADemo = () => {
+    return (
+      <div className="w-full h-[32vh] md:px-8 px-2">
+        <MovingBorders duration={10000}>
+          <div className="flex flex-col justify-between items-center gap-5">
+            <p className="block text-4xl text-sky-400 font-bold">
+              You want to know more?
+            </p>
+
+            <p className="block text-2xl">
+              Discover how Angler can transform how you manage product data and
+              accelerate your growth. <span className="text-sky-400">Because this is the future.</span>
+            </p>
+
+            <button className="relative inline-flex h-12 w-56 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 ">
+              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl hover:text-neutral-300">
+                Book A Demo
+              </span>
+            </button>
+          </div>
+        </MovingBorders>
+      </div>
+    );
+}
+
+export default BookADemo;
