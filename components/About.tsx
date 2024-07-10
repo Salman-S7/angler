45e2c2702e05b84a0072c8bf5fb18572e77ecaee
Salman-S7 <sammyzed1505@gmail.com>
2024-07-10 16:49:36 +0530

    changed padding

--- components/About.tsx
+++ components/About.tsx
@@ -4,9 +4,9 @@ import { AboutCard } from "./ui/AboutCard";
 
 const About = () => {
     return (
-      <div className="h-auto  py-12">
+      <div className="h-auto py-12  md:px-8 px-2 ">
         <AboutMarquee />
-        <div className="flex w-full lg:flex-row flex-col justify-between items-center md:px-8 px-2 gap-9 mt-9">
+        <div className="flex w-full lg:flex-row flex-col justify-between items-center gap-9 mt-9">
           {founders.map((founder) => (
             <AboutCard
               name={founder.name}
