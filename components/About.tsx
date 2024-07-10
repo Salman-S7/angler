ddee4b77813eb249263576fe5b48dd3060ac0453
Salman-S7 <sammyzed1505@gmail.com>
2024-07-10 16:22:17 +0530

    about section complete

--- components/About.tsx
+++ components/About.tsx
@@ -4,7 +4,7 @@ import { AboutCard } from "./ui/AboutCard";
 
 const About = () => {
     return (
-      <div className="h-auto">
+      <div className="h-auto  py-12">
         <AboutMarquee />
         <div className="flex w-full lg:flex-row flex-col justify-between items-center md:px-8 px-2 gap-9 mt-9">
           {founders.map((founder) => (
--- components/AboutMarquee.tsx
+++ components/AboutMarquee.tsx
@@ -4,7 +4,7 @@ import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
 const AboutMarquee = () => {
   return (
     <>
-      <p className="md:text-5xl text-3xl font-bold text-sky-400 w-full text-center py-10">
+      <p className="md:text-5xl text-3xl font-bold text-sky-400 w-full text-center pb-10">
         Built by catalog experts from
       </p>
       <div className="rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
