6946f2e31b27fd6b6c9a69498062b9e33de7b7b9
Salman-S7 <sammyzed1505@gmail.com>
2024-07-10 16:45:55 +0530

    footer complete

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,17 @@
 const Footer = () => {
-    return (
-      <div className="md:p-8 p-2 w-full h-[30vh] flex flex-col justify-between items-center">
-        <div className="w-full flex justify-between items-center">
-          <div className="text-3xl">Angler</div>
-          <div className="text-3xl">Contact us</div>
+  return (
+    <div className="md:px-8 px-2 w-full h-[24vh]">
+      <div className="h-full flex flex-col justify-between items-center  border-t border-l border-r border-gray-600 rounded-t-3xl p-8">
+        <div className="w-full flex justify-between items-center sm:text-3xl">
+          <div className="">Angler</div>
+          <div className="hover:text-sky-400">Contact us</div>
         </div>
         <div className="w-full text-center text-slate-500">
           © 2024 Angler Technologies, Inc. All rights reserved.
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Footer;
